524792549a79afef72add86ffc4a1c57e61d6e2c
luanmenezes0 <luanmenezes99@gmail.com>
2020-08-09 12:34:27 -0300

    added header navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,135 @@
-import React from 'react'
-import { Button } from 'antd'
+import React from "react";
+import logo from "./logo.png";
+import { Link } from "react-router-dom";
+import { Button, Row, Col, Divider, Menu, Dropdown, Tooltip } from "antd";
+import styles from "./Header.module.css";
+import { MessageOutlined, BellOutlined } from "@ant-design/icons";
+import Avatar from "antd/lib/avatar/avatar";
 
-const Header = (props) => {
+const Header = ({ logout, user }) => {
+  const registerLink = [
+    { item: "Alertas de SLA", route: "/" },
+    { item: "Ambientes", route: "/" },
+    { item: "Bens", route: "/" },
+    { item: "Centros de Custo", route: "/" },
+    { item: "Checklist", route: "/" },
+    { item: "Alertas de SLA", route: "/" },
+    { item: "Fornecedores", route: "/" },
+    { item: "Regras de Tickets Automáticos", route: "/" },
+    { item: "Serviços", route: "/" },
+    { item: "Tipos de Bem", route: "/" },
+    { item: "Tipos Prediais", route: "/" },
+  ];
+
+  const securityLinks = [
+    { item: "Grupos de Usuários", route: "/" },
+    { item: "Perfis de Acesso", route: "/" },
+    { item: "Usuários", route: "/" },
+  ];
+
+  const monitoringLinks = [
+    { item: "Painel de Operações", route: "/" },
+    { item: "Trílogo Now", route: "/" },
+    { item: "Trílogo TV", route: "/" },
+  ];
+
+  const ConfigLinks = [
+    { item: "Preferências de Empresa", route: "/" },
+    { item: "Preferências de Grupos", route: "/" },
+  ];
+
+  const menu = (links) => (
+    <Menu>
+      {links.map((option) => (
+        <Menu.Item>
+          <Link to={option.route}>{option.item}</Link>
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  const userMenu = () => (
+    <Menu>
+      <Menu.Item>
+        <Link to="">Perfil</Link>
+      </Menu.Item>
+      <Menu.Item>
+        <span onClick={logout}>Sair</span>
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
-    <header style={{ margin: 'auto', textAlign: 'center', padding: '40px' }}>
-
-      <Button shape="round" onClick={props.logout} type="danger">SAIR</Button>
-
+    <header>
+      <Row align="middle" justify="center">
+        <Col span={5}>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </Col>
+        <Col span={12}>
+          <nav>
+            <ul className={styles.Menu}>
+              <li>
+                <Link>Tickets</Link>
+              </li>
+              <li>
+                <Dropdown overlay={menu(registerLink)} placement="bottomCenter">
+                  <Link>Cadastro</Link>
+                </Dropdown>
+              </li>
+              <li>
+                <Link>Auditorias</Link>
+              </li>
+              <li>
+                <Link>Relatórios</Link>
+              </li>
+              <li>
+                <Dropdown
+                  overlay={menu(securityLinks)}
+                  placement="bottomCenter"
+                >
+                  <Link>Segurança</Link>
+                </Dropdown>
+              </li>
+              <li>
+                <Dropdown
+                  overlay={menu(monitoringLinks)}
+                  placement="bottomCenter"
+                >
+                  <Link>Monitoramento</Link>
+                </Dropdown>
+              </li>
+              <li>
+                <Dropdown overlay={menu(ConfigLinks)} placement="bottomCenter">
+                  <Link>Configurações</Link>
+                </Dropdown>
+              </li>
+            </ul>
+          </nav>
+        </Col>
+        <Col span={3}>
+          <div className={styles.Icons}>
+            <Tooltip placement="bottom" title="Chat">
+              <MessageOutlined className={styles.Icon} />
+            </Tooltip>
+            <Tooltip placement="bottom" title="Notificações">
+              <BellOutlined className={styles.Icon} />
+            </Tooltip>
+          </div>
+        </Col>
+        <Col span={4}>
+          <Dropdown overlay={userMenu} placement="bottomCenter">
+            <div className={styles.Avatar}>
+              <Avatar size="large" src="https://picsum.photos/100/100" />
+              <span>{user}</span>
+            </div>
+          </Dropdown>
+        </Col>
+      </Row>
+      <Divider />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/containers/Tickets/Tickets.jsx
+++ src/containers/Tickets/Tickets.jsx
@@ -2,21 +2,83 @@ import React, { Component } from "react";
 import Header from "../../components/Header/Header";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
-import { logout } from '../../redux/Auth/actions';
+import { logout } from "../../redux/Auth/actions";
+import styles from "./Tickets.module.css";
+import { Button, Radio, Empty } from "antd";
+import Modal from "antd/lib/modal/Modal";
 
 class Tickets extends Component {
+  state = {
+    visible: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
   render() {
     const ticketsPage = (
       <>
-        <Header logout={this.props.onLogoutUser} />
-        <div style={{ margin: 'auto', textAlign: 'center' }}>
-          <h1>Hello {this.props.location.name}!</h1>
-        </div>
-
+        <Header
+          logout={this.props.onLogoutUser}
+          user={this.props.location.name}
+        />
+        <main className={styles.Tickets}>
+          <Button type="primary" onClick={this.showModal} shape="round">
+            + Novo Ticket
+          </Button>
+          <Modal
+            className={styles.Modal}
+            style={{ top: 20, "border-radius": "18px" }}
+            title="Novo Ticket"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            footer={[
+              <Button shape="round" key="back" onClick={this.handleCancel}>
+                Cancelar
+              </Button>,
+              <Button
+                shape="round"
+                key="submit"
+                type="primary"
+                onClick={this.handleOk}
+              >
+                Criar Ticket
+              </Button>,
+            ]}
+          >
+            <Radio.Group size="normal">
+              <Radio.Button value="large">Large</Radio.Button>
+              <Radio.Button value="default">Default</Radio.Button>
+              <Radio.Button value="small">Small</Radio.Button>
+            </Radio.Group>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </Modal>
+          <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+        </main>
       </>
     );
-    return this.props.isAuth ? ticketsPage : <Redirect to="/auth" />;
+    /* return ticketsPage; */
+    return this.props.isAuth ? ticketsPage : <Redirect to="/auth" />
   }
 }
 
@@ -24,10 +86,8 @@ const mapStateToProps = (state) => ({
   isAuth: state.auth.isAuth,
 });
 
-
 const mapDispatchToProps = (dispatch) => ({
-  onLogoutUser: () => dispatch(logout())
-})
-
+  onLogoutUser: () => dispatch(logout()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Tickets);
